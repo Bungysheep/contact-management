86fff90ed54f2dd7f7a53b7b20890d93b9761411
James Embongbulan <james.embongbulan@gmail.com>
2019-10-10 21:16:25 +0800

    Initial changes of communication method

--- scripts/protoc-gen.sh
+++ scripts/protoc-gen.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:. audit.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:. contactsystem.proto
 protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:. communicationmethod.proto
